381fb87b10f03e3237922a935de7cf34e9c01213
BARS2021 <84984889+BARS2021@users.noreply.github.com>
2021-07-15 14:54:46 -0700

    Add files via upload

--- collapsible.js
+++ collapsible.js
@@ -2,13 +2,12 @@ var collapsible = document.getElementsByClassName("collapsible");
 
 for (var index = 0; index < collapsible.length; index++) {
   function expand() {
-    var collapsibleContent = this.nextElementSibling;
-    if (collapsibleContent.style.maxHeight){
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
       this.style.color = "white";
       this.style.backgroundColor = "black";
       content.style.maxHeight = null;
-    }
-    else {
+    } else {
       this.style.color = "black";
       this.style.backgroundColor = "#00ff00";
       content.style.maxHeight = content.scrollHeight + "px";
