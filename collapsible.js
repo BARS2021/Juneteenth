f521813d5e33da05dbad579c0f7a20afbf4276d4
BARS2021 <84984889+BARS2021@users.noreply.github.com>
2021-07-15 10:07:28 -0700

    Add files via upload

--- collapsible.js
+++ collapsible.js
@@ -1,15 +1,17 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+var collapsible = document.getElementsByClassName("collapsible");
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+for (var index = 0; index < collapsible.length; index++) {
+  function expand() {
     var content = this.nextElementSibling;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
+      this.style.color = "white";
+      this.style.backgroundColor = "black";
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
-
+      this.style.color = "black";
+      this.style.backgroundColor = "#00ff00";
     }
-  });
+  }
+  collapsible[index].addEventListener("click", expand);
 }
--- scrollIndicator.js
+++ scrollIndicator.js
@@ -1,9 +1,7 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
+window.onscroll = function indicateScroll() {
+  var scrollTotal = document.documentElement.scrollHeight;
+  var scrolledAmount = document.documentElement.scrollTop;
+  var scrollPosition = scrollTotal - document.documentElement.clientHeight;
+  var scrollPercentage = (scrolledAmount / scrollPosition) * 100;
+  document.getElementById("scrollIndicator").style.width = scrollPercentage + "%";
+};
