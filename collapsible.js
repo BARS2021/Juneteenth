d6fe8dbcee7d4f8b067bffe48311c5285dfeed9e
BARS2021 <84984889+BARS2021@users.noreply.github.com>
2021-07-15 10:27:24 -0700

    Add files via upload

--- collapsible.js
+++ collapsible.js
@@ -4,13 +4,13 @@ for (var index = 0; index < collapsible.length; index++) {
   function expand() {
     var content = this.nextElementSibling;
     if (content.style.maxHeight){
-      content.style.maxHeight = null;
       this.style.color = "white";
       this.style.backgroundColor = "black";
+      content.style.maxHeight = null;
     } else {
-      content.style.maxHeight = content.scrollHeight + "px";
       this.style.color = "black";
       this.style.backgroundColor = "#00ff00";
+      content.style.maxHeight = content.scrollHeight + "px";
     }
   }
   collapsible[index].addEventListener("click", expand);
--- scrollIndicator.js
+++ scrollIndicator.js
@@ -1,7 +1,7 @@
 window.onscroll = function indicateScroll() {
   var scrollTotal = document.documentElement.scrollHeight;
   var scrolledAmount = document.documentElement.scrollTop;
-  var scrollPosition = scrollTotal - document.documentElement.clientHeight;
-  var scrollPercentage = (scrolledAmount / scrollPosition) * 100;
+  var scrollHeight = scrollTotal - document.documentElement.clientHeight;
+  var scrollPercentage = (scrolledAmount / scrollHeight) * 100;
   document.getElementById("scrollIndicator").style.width = scrollPercentage + "%";
 };
